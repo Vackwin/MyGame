ffc691bc5294385ad358d2500ad20ec50773cfa9
Brian <ad715310@gmail.com>
2021-06-23 20:48:53 +0800

    MyGameView as a library

--- app/src/main/java/com/example/android/mygame/PlayGameFragment.kt
+++ app/src/main/java/com/example/android/mygame/PlayGameFragment.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.findNavController
+import com.example.android.mygamelibrary.MyGameView
+import com.example.android.mygamelibrary.Prize
 
 class PlayGameFragment : Fragment() {
     override fun onCreateView(
--- app/src/main/java/com/example/android/mygame/Prize.kt
+++
@@ -1,4 +0,0 @@
-package com.example.android.mygame
-import android.graphics.Bitmap
-
-class Prize (val probability : Float, val img : Bitmap)
